bee51de098efa7b250d96b7de73377f666f0450f
Piyushh4 <120736971+Piyushh4@users.noreply.github.com>
2024-03-28 16:48:21 +0530

    index.js

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ app.get('/', (req, res) => {
   res.send('YaY Your Bot Status Changed✨');
 });
 app.listen(port, () => {
-  console.log(`🔗 Listening to RTX: http://localhost:${port}`);
-  console.log(`🔗 Powered By RTX`);
+  console.log(`🔗 Listening to pitush: http://localhost:${port}`);
+  console.log(`🔗 Powered By pitush`);
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Praying God Choco","Playing Chess"];
 
 
 let currentIndex = 0;
